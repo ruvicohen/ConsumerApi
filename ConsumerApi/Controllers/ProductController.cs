4d4117bd93e6453395c29a059901765b34fb19e8
reuven <r0583247229@gmail.com>
2024-08-15 17:23:01 +0300

    Add project files.

---
+++ ConsumerApi/Controllers/ProductController.cs
@@ -0,0 +1,104 @@
+﻿using ConsumerApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ConsumerApi.Service;
+
+namespace ConsumerApi.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly CrudService _productService;
+
+    public ProductController(CrudService productService)
+    {
+        _productService = productService;
+    }
+    // GET: ProductController
+    public ActionResult Index()
+    {
+        var products = _productService.GetAllProductsAsync();
+        return View(products);
+    }
+
+    // GET: ProductController/Details/5
+    public ActionResult Details(int id)
+    {
+        var product = _productService.GetProductByIdAsync(id);
+        if (product == null)
+            return NotFound();
+
+        return View(product);
+    }
+
+    // GET: ProductController/Create
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: ProductController/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            _productService.CreateProductAsync(product);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(product);
+    }
+
+    // GET: ProductController/Edit/5
+    public ActionResult Edit(int id)
+    {
+        return View();
+    }
+
+    // POST: ProductController/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, Product product)
+    {
+        if (id != product.Id)
+            return BadRequest();
+
+        if (ModelState.IsValid)
+        {
+            _productService.UpdateProductAsync(id, product);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(product);
+    }
+    // GET: ProductController/Delete/5
+    public IActionResult Delete(int id)
+    {
+        var product = _productService.GetProductByIdAsync(id);
+        if (product == null)
+            return NotFound();
+
+        return View(product);
+    }
+    // POST: ProductController/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        _productService.DeleteProductAsync(id);
+        return RedirectToAction(nameof(Index));
+    }
+}
+
+    
+
+    
+    
+
+    
+
+    
+
+    
+
+  
+
---
+++ ConsumerApi/Service/CrudService.cs
@@ -0,0 +1,43 @@
+﻿using ConsumerApi.Models;
+
+namespace ConsumerApi.Service
+{
+    public class CrudService
+    {
+       
+            private readonly HttpClient _httpClient;
+
+            public CrudService(HttpClient httpClient)
+            {
+                _httpClient = httpClient;
+            }
+
+            public async Task<List<Product>> GetAllProductsAsync()
+            {
+                return await _httpClient.GetFromJsonAsync<List<Product>>("https://fakestoreapi.com/products");
+            }
+
+            public async Task<Product> GetProductByIdAsync(int id)
+            {
+                return await _httpClient.GetFromJsonAsync<Product>($"https://fakestoreapi.com/products/{id}");
+            }
+
+            public async Task<Product> CreateProductAsync(Product newProduct)
+            {
+                var response = await _httpClient.PostAsJsonAsync("https://fakestoreapi.com/products", newProduct);
+                return await response.Content.ReadFromJsonAsync<Product>();
+            }
+
+            public async Task UpdateProductAsync(int id, Product updatedProduct)
+            {
+                await _httpClient.PutAsJsonAsync($"https://fakestoreapi.com/products/{id}", updatedProduct);
+            }
+
+            public async Task DeleteProductAsync(int id)
+            {
+                await _httpClient.DeleteAsync($"https://fakestoreapi.com/products/{id}");
+            }
+        }
+    }
+
+
