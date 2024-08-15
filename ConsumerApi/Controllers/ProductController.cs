5c96103666eb261a4337d755a101a964d160cb37
reuven <r0583247229@gmail.com>
2024-08-15 17:41:33 +0300

    ש

--- ConsumerApi/Controllers/ProductController.cs
+++ ConsumerApi/Controllers/ProductController.cs
@@ -1,104 +1,97 @@
 ﻿using ConsumerApi.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ConsumerApi.Service;
+using System.Threading.Tasks; // הוסף את ה-namespace הזה
 
-namespace ConsumerApi.Controllers;
-
-public class ProductController : Controller
+namespace ConsumerApi.Controllers
 {
-    private readonly CrudService _productService;
-
-    public ProductController(CrudService productService)
+    public class ProductController : Controller
     {
-        _productService = productService;
-    }
-    // GET: ProductController
-    public ActionResult Index()
-    {
-        var products = _productService.GetAllProductsAsync();
-        return View(products);
-    }
+        private readonly CrudService _productService;
 
-    // GET: ProductController/Details/5
-    public ActionResult Details(int id)
-    {
-        var product = _productService.GetProductByIdAsync(id);
-        if (product == null)
-            return NotFound();
-
-        return View(product);
-    }
-
-    // GET: ProductController/Create
-    public ActionResult Create()
-    {
-        return View();
-    }
-
-    // POST: ProductController/Create
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public IActionResult Create(Product product)
-    {
-        if (ModelState.IsValid)
+        public ProductController(CrudService productService)
         {
-            _productService.CreateProductAsync(product);
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAllProductsAsync();
+            return View(products);
+        }
+
+        // GET: ProductController/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var product = await _productService.GetProductByIdAsync(id);
+            if (product == null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        // GET: ProductController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.CreateProductAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: ProductController/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product = await _productService.GetProductByIdAsync(id);
+            if (product == null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Product product)
+        {
+            if (id != product.Id)
+                return BadRequest();
+
+            if (ModelState.IsValid)
+            {
+                await _productService.UpdateProductAsync(id, product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: ProductController/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _productService.GetProductByIdAsync(id);
+            if (product == null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _productService.DeleteProductAsync(id);
             return RedirectToAction(nameof(Index));
         }
-        return View(product);
-    }
-
-    // GET: ProductController/Edit/5
-    public ActionResult Edit(int id)
-    {
-        return View();
-    }
-
-    // POST: ProductController/Edit/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public IActionResult Edit(int id, Product product)
-    {
-        if (id != product.Id)
-            return BadRequest();
-
-        if (ModelState.IsValid)
-        {
-            _productService.UpdateProductAsync(id, product);
-            return RedirectToAction(nameof(Index));
-        }
-        return View(product);
-    }
-    // GET: ProductController/Delete/5
-    public IActionResult Delete(int id)
-    {
-        var product = _productService.GetProductByIdAsync(id);
-        if (product == null)
-            return NotFound();
-
-        return View(product);
-    }
-    // POST: ProductController/Delete/5
-    [HttpPost, ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    public IActionResult DeleteConfirmed(int id)
-    {
-        _productService.DeleteProductAsync(id);
-        return RedirectToAction(nameof(Index));
     }
 }
-
-    
-
-    
-    
-
-    
-
-    
-
-    
-
-  
-
--- ConsumerApi/Service/CrudService.cs
+++ ConsumerApi/Service/CrudService.cs
@@ -1,43 +1,42 @@
 ﻿using ConsumerApi.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ConsumerApi.Service
 {
     public class CrudService
     {
-       
-            private readonly HttpClient _httpClient;
+        private readonly HttpClient _httpClient;
 
-            public CrudService(HttpClient httpClient)
-            {
-                _httpClient = httpClient;
-            }
+        public CrudService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
 
-            public async Task<List<Product>> GetAllProductsAsync()
-            {
-                return await _httpClient.GetFromJsonAsync<List<Product>>("https://fakestoreapi.com/products");
-            }
+        public async Task<List<Product>> GetAllProductsAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Product>>("https://fakestoreapi.com/products");
+        }
 
-            public async Task<Product> GetProductByIdAsync(int id)
-            {
-                return await _httpClient.GetFromJsonAsync<Product>($"https://fakestoreapi.com/products/{id}");
-            }
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<Product>($"https://fakestoreapi.com/products/{id}");
+        }
 
-            public async Task<Product> CreateProductAsync(Product newProduct)
-            {
-                var response = await _httpClient.PostAsJsonAsync("https://fakestoreapi.com/products", newProduct);
-                return await response.Content.ReadFromJsonAsync<Product>();
-            }
+        public async Task<Product> CreateProductAsync(Product newProduct)
+        {
+            var response = await _httpClient.PostAsJsonAsync("https://fakestoreapi.com/products", newProduct);
+            return await response.Content.ReadFromJsonAsync<Product>();
+        }
 
-            public async Task UpdateProductAsync(int id, Product updatedProduct)
-            {
-                await _httpClient.PutAsJsonAsync($"https://fakestoreapi.com/products/{id}", updatedProduct);
-            }
+        public async Task UpdateProductAsync(int id, Product updatedProduct)
+        {
+            await _httpClient.PutAsJsonAsync($"https://fakestoreapi.com/products/{id}", updatedProduct);
+        }
 
-            public async Task DeleteProductAsync(int id)
-            {
-                await _httpClient.DeleteAsync($"https://fakestoreapi.com/products/{id}");
-            }
+        public async Task DeleteProductAsync(int id)
+        {
+            await _httpClient.DeleteAsync($"https://fakestoreapi.com/products/{id}");
         }
     }
-
+}
 
